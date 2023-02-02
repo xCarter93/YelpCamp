40a7525e47f6befd8689c3890dc17c9b77dc4b73
xCarter93 <xcarter93@gmail.com>
2023-02-01 22:15:40 -0500

    Added more styling

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 65 + 7);
 		const camp = new Campground({
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
+			image: "https://source.unsplash.com/collection/483251",
+			description:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean.",
+			price,
 		});
 		await camp.save();
 	}
